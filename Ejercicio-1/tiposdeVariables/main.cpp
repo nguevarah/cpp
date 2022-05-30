07877a4cf05d9b60c3624d7577d6dbc5224936cf
Nely Guevara <Cursosdecomputacion.net@gmail.com>
2022-05-30 12:10:04 -0600

    agregando ejercicio 1

---
+++ Ejercicio-1/tiposdeVariables/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    //Tipos de Variables
+    
+    
+/*  int
+    float
+    double
+    char
+    bool
+    string
+*/ 
+
+/*
+    int i = 10;
+    float f = 10.2;
+    double d = 10.2;
+    char c = 't';
+    string s = "hola";
+
+    string nombre = "jose";
+
+    cout<< "Hola";
+    cin>> nombre;
+    cout<< nombre;
+*/
+
+int a = 10;
+int b = 8;
+if (a < b)
+{
+    cout << " a es mayor que b";
+}
+else
+{
+    //false
+}
+
+
+    return 0;
+}
+
