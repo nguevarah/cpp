9bf3876e7743291a8034cfc6c4e727c90419878b
Nely Guevara <Cursosdecomputacion.net@gmail.com>
2022-06-06 08:39:30 -0600

    agregando ciclo infinito

---
+++ ejercicios/Ejercicio 17 ciclo while infinito/cicloInfinito.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool esCierto = true;
+
+    if (esCierto)
+    {
+    
+    }
+
+    if (true)
+    {
+        
+    }
+
+    while (true)
+    {
+        cout << " * ";
+        
+    }
+    
+    
+    return 0;
+}
