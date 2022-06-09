d270f51d3801f1c8085795457d44e5777e2caa64
Nely Guevara <Cursosdecomputacion.net@gmail.com>
2022-06-09 09:05:33 -0600

    do while

--- ejercicios/Ejercicio 19 Do While/main.cpp
+++ ejercicios/Ejercicio 19 Do While/main.cpp
@@ -7,11 +7,13 @@ int main(int argc, char const *argv[])
     int a = 2;
     int b = 10;
 
+    // primero pregunta y despues ejecuta
     while(a > b){
         cout << " (while) a > b " << endl;
         break;
     }
 
+    // primero ejecuta y despues pregunta
     do{
         cout << " (do while) a > b " << endl;
         break;
