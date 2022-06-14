42aacf2fc530ebe9500f5a6fb744723d3f5c70c5
Nely Guevara <Cursosdecomputacion.net@gmail.com>
2022-06-14 09:43:36 -0600

    agregando proyecto 1, unidad 1 modificado

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -4,12 +4,39 @@
 
 using namespace std;
 
+// primera manera de hacerlo
+
 int main(int argc, char const *argv[])
 {
+    int opcion = 0;
+
+    cout << " ************** " << endl;
+    cout << " MENU DE JUEGOS " << endl;
+    cout << " ************** " << endl;
+
+    cout << endl;
+
+    cout << " Seleccione un juego " << endl << endl;
+    cout << " 1 StarShip " << endl;
+    cout << " 2 Snake " << endl << endl;
+    cout << " Ingrese un numero del menu para seleccionar un juego: ";
+    cin >> opcion;
+
     system("cls");
 
-    // starShip();
-    // snake();
+    if (opcion == 1)
+    {
+        starShip();
+    }
 
+    if (opcion == 2)
+    {
+        snake();
+    }
+    else
+        {
+            cout << " Error! Recuerda ingresar una de las opciones del menu ";
+        }
+   
     return 0;
 }
